453dd94b05c8ee636ed84c5dca12e036c1c4e2e9
w1spuchy <a.shiroky2005@gmail.com>
2026-04-21 12:18:10 +0300

    add: added discount for search results

--- scripts/block_scripts/search.js
+++ scripts/block_scripts/search.js
@@ -84,20 +84,42 @@ export async function initSearch()
                                                             border-radius: 8px;">
                         </div>
                         <div class='search-res-item-info-section'>
-                            <div style="font-size: 16px; font-weight: 600;">
+                            <div class="top-info-section" style="font-size: 16px; font-weight: 600;">
                                 ${res.name}
                             </div>
-                            <div style="display: flex; flex-direction:row; justify-content: space-between;">
-                                <div style="font-weight: 600; color: #155dfc">
-                                    $${res.price}
-                                </div>
-                                <div style="color: #fdc700;">
-                                    ${renderRating(res.rating)}
-                                </div>
+                            <div class="bot-info-section" style="display: flex; flex-direction:row; justify-content: space-between;">
                             </div>
                         </div>
                     </a>
                 `
+                if(!res.discount)
+                {
+                    productItem.querySelector('.bot-info-section').innerHTML = 
+                    `
+                        <div style="font-weight: 600; color: #155dfc">
+                            $${res.price}
+                        </div>
+                        <div style="color: #fdc700;">
+                            ${renderRating(res.rating)}
+                        </div>
+                    `
+                }
+                else
+                {
+                     productItem.querySelector('.bot-info-section').innerHTML =
+                     `
+                        <div style="display: flex; flex-direction: row; align-items: center; gap: 4px">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="red" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-badge-percent-icon lucide-badge-percent"><path d="M3.85 8.62a4 4 0 0 1 4.78-4.77 4 4 0 0 1 6.74 0 4 4 0 0 1 4.78 4.78 4 4 0 0 1 0 6.74 4 4 0 0 1-4.77 4.78 4 4 0 0 1-6.75 0 4 4 0 0 1-4.78-4.77 4 4 0 0 1 0-6.76Z"/><path d="m15 9-6 6"/><path d="M9 9h.01"/><path d="M15 15h.01"/></svg>
+                            <div style="font-weight: 600; color: red">
+                                $${(res.price * (1 - res.discount)).toFixed(2)}
+                            </div>
+                        </div>
+                        <div style="color: #fdc700;">
+                            ${renderRating(res.rating)}
+                        </div>
+                     `
+                }
+
                 searchResultList.appendChild(productItem);
             })
         }
