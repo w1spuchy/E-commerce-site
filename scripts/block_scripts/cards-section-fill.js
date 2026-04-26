0a4fa94b94429f261a59f347f37234e1094bbe0b
w1spuchy <a.shiroky2005@gmail.com>
2026-04-26 21:50:53 +0300

    style: changed star svg

--- scripts/block_scripts/cards-section-fill.js
+++ scripts/block_scripts/cards-section-fill.js
@@ -256,19 +256,13 @@ export function renderRating(rating)
     const ratingContainer = document.createElement('div');
     
     for(let i = 0; i < Math.floor(rating); i++){
-        const fullStar = document.createElement('i');
-        fullStar.classList = 'fas fa-star';
-        ratingContainer.appendChild(fullStar);
+        ratingContainer.innerHTML += `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="#fdc700" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-star-icon lucide-star"><path d="M11.525 2.295a.53.53 0 0 1 .95 0l2.31 4.679a2.123 2.123 0 0 0 1.595 1.16l5.166.756a.53.53 0 0 1 .294.904l-3.736 3.638a2.123 2.123 0 0 0-.611 1.878l.882 5.14a.53.53 0 0 1-.771.56l-4.618-2.428a2.122 2.122 0 0 0-1.973 0L6.396 21.01a.53.53 0 0 1-.77-.56l.881-5.139a2.122 2.122 0 0 0-.611-1.879L2.16 9.795a.53.53 0 0 1 .294-.906l5.165-.755a2.122 2.122 0 0 0 1.597-1.16z"/></svg>`
     }
     if(rating % 1 != 0){
-        const halfStar = document.createElement('i');
-        halfStar.classList = 'fas fa-star-half-alt';
-        ratingContainer.appendChild(halfStar);
+        ratingContainer.innerHTML += `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="#fdc700" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-star-half-icon lucide-star-half"><path d="M12 18.338a2.1 2.1 0 0 0-.987.244L6.396 21.01a.53.53 0 0 1-.77-.56l.881-5.139a2.12 2.12 0 0 0-.611-1.879L2.16 9.795a.53.53 0 0 1 .294-.906l5.165-.755a2.12 2.12 0 0 0 1.597-1.16l2.309-4.679A.53.53 0 0 1 12 2"/></svg>`
     }
     for(let i = 0; i < (5-Math.ceil(rating)); i++){
-        const emptyStar = document.createElement('i');
-        emptyStar.classList = 'far fa-star';
-        ratingContainer.appendChild(emptyStar);
+        ratingContainer.innerHTML += `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="oklch(0.93 0.01 0)" stroke="oklch(0.93 0.01 0)" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-star-icon lucide-star"><path d="M11.525 2.295a.53.53 0 0 1 .95 0l2.31 4.679a2.123 2.123 0 0 0 1.595 1.16l5.166.756a.53.53 0 0 1 .294.904l-3.736 3.638a2.123 2.123 0 0 0-.611 1.878l.882 5.14a.53.53 0 0 1-.771.56l-4.618-2.428a2.122 2.122 0 0 0-1.973 0L6.396 21.01a.53.53 0 0 1-.77-.56l.881-5.139a2.122 2.122 0 0 0-.611-1.879L2.16 9.795a.53.53 0 0 1 .294-.906l5.165-.755a2.122 2.122 0 0 0 1.597-1.16z"/></svg>`
     }
 
     return ratingContainer.innerHTML;
